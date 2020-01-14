7e2a74c98b9e93ef3bb5dcb018d9153e3e82c710
zholderness3 <zholderness3@gatech.edu>
2020-01-14 12:18:57 -0500

    Initial Commit

---
+++ scouter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  scouter
+//
+//  Created by Zoe Holderness on 1/14/20.
+//  Copyright © 2020 Zoe Holderness. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
